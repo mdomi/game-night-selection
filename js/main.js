62982a74509047a39d4ba953e3d0911f29847cf2
Mike Dominice <mike@mikedominice.com>
2014-05-07 17:26:16 -0400

    initial app demo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
 require.config({
     paths : {
-        'text' : '../bower_components/requirejs-text/text',
-        'jquery' : '../bower_components/jquery/dist/jquery.min'
+        'angular' : [
+            '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.15/angular.min',
+            '../bower_components/angular/angular.min'
+        ],
+        'text' : [
+            '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text',
+            '../bower_components/requirejs-text/text'
+        ],
+        'jquery' : [
+            '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
+            '../bower_components/jquery/dist/jquery.min'
+        ]
+    },
+    shim : {
+        'angular' : {
+            exports : 'angular',
+            deps : ['jquery']
+        }
     }
 });
 
-require(['jquery', 'text!../data/games.json'], function ($, games) {
-    $(function () {
-        window.console.log(JSON.parse(games));
+require(['angular', 'text!../data/games.json'], function (angular, games) {
+    'use strict';
+
+    angular.module('games', [])
+        .controller('GamesController', ['$scope', function ($scope) {
+            $scope.games = JSON.parse(games).games;
+        }]);
+    
+    angular.element().ready(function () {
+        angular.bootstrap(document, ['games']);
     });
 });
