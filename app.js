51aba14697b0a978e7315ce7ac1cadf4419e4df0
Mike Dominice <mike@mikedominice.com>
2014-05-21 11:51:48 -0400

    added start script

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var path = require('path');
 
 var express = require('express'),
+    connect = require('connect'),
     compress = require('compression');
 
 var config = require('./config');
 
 var app = express();
 
-app.use(express.logger());
+app.use(connect.logger());
 app.use(compress());
 app.use(express.static(path.join(__dirname, 'www')));
 
