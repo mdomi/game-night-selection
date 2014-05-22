dd3cc0a5c90b45dd215505e7d4f5f39f19921dda
Mike Dominice <mike@mikedominice.com>
2014-05-21 21:11:57 -0400

    fixed livereload in production

--- config/production.js
+++ config/production.js
@@ -4,15 +4,6 @@ module.exports = {
     server : {
         port : PORT
     },
-    grunt : {
-        connect : {
-            hostname : 'localhost',
-            port : PORT
-        },
-        livereload : {
-            port : 35730
-        }
-    },
     auth : {
         google : {
             clientId : process.env.GOOGLE_CLIENT_SECRET
