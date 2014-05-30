7c3fe11efa9be27816b151db1f06c4a09d0d573c
Mike Dominice <mike@mikedominice.com>
2014-05-30 12:24:32 -0400

    added ownedBy filter

--- public/js/controllers/gamescontroller.js
+++ public/js/controllers/gamescontroller.js
@@ -1,17 +1,36 @@
 define([
+    'underscore',
+    'angular',
     'modules/games',
     'services/gamesservice',
     'services/eventsservice',
     'filters/playerrange',
+    'filters/ownedby',
     'filters/duration',
     'filters/join'
-], function (games) {
+], function (_, angular, games) {
 
     function GamesController($scope, GamesService, EventsService) {
         GamesService.getGames().then(function (games) {
             $scope.games = games;
         });
         $scope.events = EventsService.listEvents();
+        $scope.potentialOwners = ['Mike', 'Scott', 'Brad', 'Jerry'];
+
+        $scope.ownerFilter = $scope.potentialOwners.reduce(function (filter, owner) {
+            filter[owner] = true;
+            return filter;
+        }, {});
+
+        function ownerIsSelected(owner) {
+            return $scope.ownerFilter[owner] === true;
+        }
+
+        $scope.$watch(function () {
+            return Object.keys($scope.ownerFilter).filter(ownerIsSelected).sort().join(',');
+        }, function (newOwners) {
+            $scope.owners = newOwners;
+        });
     }
 
     games.controller('GamesController', ['$scope', 'GamesService', 'EventsService', GamesController]);
